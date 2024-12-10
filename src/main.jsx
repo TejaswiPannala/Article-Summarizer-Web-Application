eb56bb564307e5bf8a7389576286ced90f9aab5f
TejaswiPannala <165891145+TejaswiPannala@users.noreply.github.com>
2024-12-10 22:57:17 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 // main.jsx or App.jsx
 import React from 'react'; // Add this line
 import ReactDOM from 'react-dom/client';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>  {/* Wrap the App component with HashRouter */}
+      <App />
+    </Router>
   </React.StrictMode>
 );
